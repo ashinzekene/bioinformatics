201149d2b06a332f7ec4b56c6ba2de3d34bee6d6
Ekene Ashinze <ashinzekene@gmail.com>
2021-01-15 12:23:25 +0100

    Fix issues with middle edge algorithm

--- 3-comparing-genes-proteins-and-genomes/week_3/middle_edge.py
+++ 3-comparing-genes-proteins-and-genomes/week_3/middle_edge.py
@@ -4,11 +4,13 @@ def GetScoreMatrix(v, w, indel):
     lv = len(v)
     lw = len(w)
     previous_matrix = [0] * (lv+1)
-    current_matrix = [-indel] * (lv+1)
+    current_matrix = [0] * (lv+1)
     for i in range(1, lv+1):
-        previous_matrix[i] = previous_matrix[i-1] - indel
+        current_matrix[i] = current_matrix[i-1] - indel
 
     for j in range(1, lw+1):
+        previous_matrix = current_matrix.copy()
+        current_matrix[0] = -indel * j
         for i in range(1, lv+1):
             v_val = v[i-1]
             w_val = w[j-1]
@@ -17,20 +19,9 @@ def GetScoreMatrix(v, w, indel):
             deletion = current_matrix[i-1] - indel
             match_mismatch = previous_matrix[i-1] + Blosom_62[v_val][w_val]
             current_matrix[i] = max(insertion, deletion, match_mismatch)
-        if j < lw:
-            previous_matrix = [i for i in current_matrix]
-            # Don't switch matrices at the end, we need both
 
     matrix = [[previous_matrix[i], current_matrix[i]] for i in range(lv+1)] 
     return matrix
-
-def GetMiddleNode(middle_column):
-    mid_val, mid_index = middle_column[0], 0
-    for i, val in enumerate(middle_column):
-        if val > mid_val:
-            mid_val = val
-            mid_index = i
-    return mid_index
 
 
 def MiddleEdge(v, w, indel):
@@ -38,12 +29,12 @@ def MiddleEdge(v, w, indel):
     middle_col_index = lw//2
 
     from_source = GetScoreMatrix(v, w[0:middle_col_index+1], indel)
-    from_sink = GetScoreMatrix(v, w[lw:middle_col_index-2:-1], indel)
+    from_sink = GetScoreMatrix(v, w[lw:middle_col_index-1:-1], indel)
 
     middle_column = [from_source[i][0] + from_sink[i][1] for i in range(len(v)+1)]
-    next_column = [from_source[i][1] + from_sink[i][1] for i in range(len(v)+1)]
+    next_column = [from_source[i][1] for i in range(len(from_source))]
 
-    mid_index = GetMiddleNode(middle_column)
+    mid_index = middle_column.index(max(middle_column))
 
     insertion = next_column[mid_index]
     deletion = middle_column[mid_index + 1]
@@ -61,8 +52,8 @@ def MiddleEdge(v, w, indel):
 
 
 if __name__ == "__main__":
-    v = "PLEASANTLY"
-    w = "MEASNLY"
+    v = "FPPF"
+    w = "FFPF"
     indel = 5
     path = './datasets/dataset_250_12.txt'
     with open(path) as f:
